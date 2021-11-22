ebd16db9bcd09d82f039bfe5a312d3bda0d302f3
VinhD4623 <93961377+VinhD4623@users.noreply.github.com>
2021-11-21 20:20:41 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,47 +6,8 @@
 
 using namespace std;
 
-int main()
-{
-
-    /*
-    Address obj;
-    obj.read_address();
-    obj.display();
-    obj.edit_address();
-    obj.display();
-    
-    Member obj1;
-    obj1.read_member();
-    obj1.display();
-    obj1.edit_member();
-    obj1.display();
-
-    
-    Provider obj2;
-    obj2.read_provider();
-    obj2.display();
-    obj2.edit_provider();
-    obj2.display();
-    */
-   ProviderDirectory list;
-   list.addProvider();
-   list.addProvider();
-   list.addProvider();
-   cout << endl << endl;
-   list.printList();
-   list.removeProvider();
-   list.printList();
-   list.writeToFile();
-    
-   return 0;
-=======
-
-using namespace std;
-
 int main(){
 
     
     return 0;
->>>>>>> 83a9c39eeee3c5b27097155c1cb1274304c32b5b
 }
