ae6751decdc76fcc635aefee692e2a1a86fd024d
cpod2 <cpod2@pdx.edu>
2021-12-01 16:57:39 -0800

    added memberdirectory.h

---
+++ memberdirectory.h
@@ -0,0 +1,48 @@
+#ifndef MEMBERDIRECTORY_H
+#define MEMBERDIRECTORY_H
+
+#include <iostream>
+#include <list>
+#include <fstream>
+#include "info.h"
+#include <string>
+
+using namespace std;
+
+
+
+struct mnode
+{
+    mnode();
+    ~mnode();
+    Member member;              
+    psd_node * head;    //List of services that member recieved
+    mnode * next;
+};
+
+class MemberDirectory
+{
+    public: 
+        MemberDirectory();
+        ~MemberDirectory();
+
+        //Directory list manipulations
+        int addMember();
+        int removeMember();
+        int clear();
+        int editMember();
+        int printList();
+        int searchMember();
+
+        int validate(const int);
+        
+        //Provider Directory file read/write
+        int writeToFile();
+        int loadDirectory();
+
+
+    private:
+
+};
+
+#endif
